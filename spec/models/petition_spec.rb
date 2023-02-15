e37e7655b87357e9339a9a6751b40cb704a87a7d
Isaac Alter <i_alter@yahoo.com>
2023-02-15 15:00:19 -0700

    Added validation tests in petition_spec

--- spec/models/petition_spec.rb
+++ spec/models/petition_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Petition, type: :model do
     it { should have_many(:petition_pets) }
     it { should have_many(:pets).through(:petition_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_numericality_of(:zip_code) }
+  end
 end
